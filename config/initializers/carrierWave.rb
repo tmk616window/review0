7a39184609cd8a9873efe840d1c6cfd56c74228e
EC2 Default User <ec2-user@ip-172-31-38-114.ap-northeast-1.compute.internal>
2020-04-24 20:07:28 +0000

    image

---
+++ config/initializers/carrierWave.rb
@@ -0,0 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'tmk616window'
+    config.asset_host = 'https://s3.amazonaws.com/tmk616window'
+    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+    config.fog_public = false # ←コレ
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIA57SARZ7UGJGYT34L',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1',
+      # path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
